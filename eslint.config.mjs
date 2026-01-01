8f9975805613a57a214880066735343459419950
周义竣 <zhouyijun@chagee.com>
2026-01-01 23:17:07 +0800

    chore: 初始化项目配置
    添加 husky、lint-staged、commitlint、prettier 和 eslint 配置
    设置 pnpm 工作区
    添加基础 gitignore 和测试文件

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import pluginPrettier from 'eslint-plugin-prettier';
+import configPrettier from 'eslint-config-prettier';
+
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    plugins: {
+      'react-hooks': pluginReactHooks,
+      prettier: pluginPrettier,
+    },
+    rules: {
+      ...pluginReactHooks.configs.recommended.rules,
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+    },
+  },
+  configPrettier,
+];

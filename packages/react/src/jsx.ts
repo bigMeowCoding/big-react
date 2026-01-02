487a37fa6b056d15ea4c14fc5307b6b8876fe74b
周义竣 <zhouyijun@chagee.com>
2026-01-02 16:14:07 +0800

    feat(jsx): 实现独立的 jsxDev 函数以优化开发环境处理
    将原本直接引用 jsx 的 jsxDev 改为独立实现，专门处理开发环境下的元素创建。这样可以更精确地控制 key 和 ref 的处理逻辑，避免潜在的问题。

--- packages/react/src/jsx.ts
+++ packages/react/src/jsx.ts
@@ -51,4 +51,22 @@ export function jsx(type: ElementType, config: any, ...maybeChildren: any) {
   return createElement(type, key, ref, props);
 }
 
-export const jsxDev = jsx;
+export function jsxDev(type: ElementType, config: any) {
+  let key: Key = null;
+  let ref: Ref = null;
+  const props: Props = {};
+
+  for (const prop in config) {
+    if (prop === 'key' && config[prop] !== undefined) {
+      key = config.key;
+    }
+    if (prop === 'ref' && config[prop] !== undefined) {
+      ref = config.ref;
+    }
+
+    if ({}.hasOwnProperty.call(config, prop)) {
+      props[prop] = config[prop];
+    }
+  }
+  return createElement(type, key, ref, props);
+}

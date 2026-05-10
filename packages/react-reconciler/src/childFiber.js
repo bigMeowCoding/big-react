dfa056fe986110c25036156f84169769cc475f17
周义竣 <zhouyijun@chagee.com>
2026-05-10 18:47:22 +0800

    feat(react-reconciler): 实现子节点数组协调与更新逻辑
    新增 reconcileChildrenArray 和 updateFromMap 函数，优化子节点的协调过程，支持通过键值映射更新和删除子节点。改进了对文本和元素的处理逻辑，确保在更新时能够复用旧的 fiber 节点，提升了子节点协调的效率和稳定性。

--- packages/react-reconciler/src/childFiber.js
+++ packages/react-reconciler/src/childFiber.js
@@ -61,6 +61,96 @@ function ChildReconciler(shouldTrackEffects) {
     fiber.return = returnFiber;
     return fiber;
   }
+
+  function reconcileChildrenArray(returnFiber, currentFirstChild, newChildren) {
+    let lastPlacedIndex = 0;
+    let lastNewFiber = null;
+    let firstNewFiber = null;
+
+    const existingChildren = new Map();
+    let current = currentFirstChild;
+    while (current !== null) {
+      const key = current.key !== null ? current.key : current.index;
+      existingChildren.set(key, current);
+      current = current.sibling;
+    }
+    for (let i = 0; i < newChildren.length; i++) {
+      const after = newChildren[i];
+      const newFiber = updateFromMap(returnFiber, existingChildren, i, after);
+      newFiber.return = returnFiber;
+      newFiber.index = i;
+      if (lastNewFiber === null) {
+        lastNewFiber = newFiber;
+        firstNewFiber = newFiber;
+      } else {
+        lastNewFiber.sibling = newFiber;
+        lastNewFiber = newFiber;
+      }
+      if (!shouldTrackEffects) {
+        continue;
+      }
+      const current = newFiber.alternate;
+      if (current !== null) {
+        const oldIndex = current.index;
+        if (oldIndex < lastPlacedIndex) {
+          newFiber.flags |= Placement;
+          continue;
+        } else {
+          lastPlacedIndex = oldIndex;
+        }
+      } else {
+        newFiber.flags |= Placement;
+      }
+    }
+    existingChildren.forEach((child) => {
+      deleteChild(returnFiber, child);
+    });
+    return firstNewFiber;
+  }
+  function updateFromMap(returnFiber, existingChildren, index, element) {
+    let keyToUse = null;
+    if (typeof element === "string") {
+      keyToUse = index;
+    } else {
+      keyToUse = element.key === null ? index : element.key;
+    }
+    const before = existingChildren.get(keyToUse);
+    if (typeof element === "string") {
+      if (before) {
+        existingChildren.delete(keyToUse);
+        if (before.tag === HostText) {
+          const existing = useFiber(before, { content: element });
+          existing.return = returnFiber;
+          return existing;
+        } else {
+          deleteChild(returnFiber, before);
+        }
+      }
+      return new FiberNode(HostText, { content: element }, null);
+    }
+    if (typeof element === "object" && element !== null) {
+      switch (element.$$typeof) {
+        case REACT_ELEMENT_TYPE:
+          if (before) {
+            existingChildren.delete(keyToUse);
+            if (before.type === element.type) {
+              const existing = useFiber(before, element.props);
+              existing.return = returnFiber;
+              return existing;
+            } else {
+              deleteChild(returnFiber, before);
+            }
+          }
+          return createFiberFromElement(element);
+        default:
+          console.warn("未实现的reconcile类型", element);
+          break;
+      }
+    }
+    console.warn("未实现的reconcile类型", element);
+    return null;
+  }
+
   return function reconcileChildFibers(
     returnFiber,
     currentFirstChild,
